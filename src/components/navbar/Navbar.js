3e4f2e7c8e4dfbf2b2ec8cbc610c7e4bfe1d48f5
lina mierkE <lina_maria89@hotmail.com>
2021-05-04 14:40:12 -0400

    final nav

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,8 +2,10 @@ import "./navbar.css";
 export default function NavBar() {
   return (
     <div className="container">
-      
+      <div className="topInfo"> 
       <img className="logo" src="https://i.imgur.com/mMNUC5g.png" alt="logo" />
+      <h2> llamenos al 3110000000</h2>
+      </div>
       <div className="main">
         <ul className="list">
           <li> Inicio</li>
